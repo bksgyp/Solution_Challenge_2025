c28a9601fc4972b4e4866648ce18a72d6886dfbd
Seungyeop Baek <yup6786@naver.com>
2025-05-08 09:06:47 +0900

    홈페이지 기본 구조 및 컴포넌트 추가

---
+++ frontend/app/symptom/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Checkbox } from "@heroui/checkbox";
+import { Button } from "@heroui/button";
+import { Accordion, AccordionItem } from "@heroui/accordion";
+
+export default function Home() {
+  
+  return (
+    <>
+      <div> HI </div>
+    </>
+  );
+}
